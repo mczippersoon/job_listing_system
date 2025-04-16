674a5ed38b7d4d6a28e654051fc337638c417fa3
mczippersoon <mj.liba@mlgcl.edu.ph>
2025-04-16 13:29:55 +0800

    ok na

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => [ 
+    'http://127.0.0.1:5500', 
+    'http://localhost:5500'],
+    
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/userList', [UserController::class, 'list']);
+
+
+    
+// Route::match(['get', 'post'], '/login', function (Request $request) {
+//     Log::info('Method used:', ['method' => $request->method()]);
+//     return response()->json(['method' => $request->method()], 200);
+// });
